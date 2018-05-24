fd63a41e75e5485283db275d96a5410ae9223f09
bogdaniy <bogdan.gorbatenko@nextiva.com>
2018-05-24 12:45:23 +0300

    Sort messages by date & enable scrolling for messages list

--- android-app/src/containers/App/index.js
+++ android-app/src/containers/App/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
-import { Container, Tab, Tabs } from 'native-base'
+import { Container, Tab, Tabs, View } from 'native-base'
 import Message from '../../components/Message'
 import { loadMessages, markAsSpam, unMarkAsSpam } from '../../reducers/sms/smsActions'
 import { makeSelectGoodMessages, makeSelectSpamMessages } from '../../reducers/sms/smsSelectors'
@@ -36,26 +36,28 @@ class App extends React.PureComponent {
 
     return (
       <Container>
-        <Tabs>
-          <Tab heading="Inbox">
-            <MessagesList>
-              {goodMessages.map(message => <Message
-                {...message.toJS()}
-                key={message.get('id')}
-                onSpam={onSpam}
-              />).toJS()}
-            </MessagesList>
-          </Tab>
-          <Tab heading="Spam">
-            <MessagesList>
-              {spamMessages.map(message => <Message
-                {...message.toJS()}
-                key={message.get('id')}
-                onUnSpam={onUnSpam}
-              />).toJS()}
-            </MessagesList>
-          </Tab>
-        </Tabs>
+        <View style={{flex: 1}}>
+          <Tabs>
+            <Tab heading="Inbox">
+              <MessagesList>
+                {goodMessages.map(message => <Message
+                  {...message.toJS()}
+                  key={message.get('id')}
+                  onSpam={onSpam}
+                />).toJS()}
+              </MessagesList>
+            </Tab>
+            <Tab heading="Spam">
+              <MessagesList>
+                {spamMessages.map(message => <Message
+                  {...message.toJS()}
+                  key={message.get('id')}
+                  onUnSpam={onUnSpam}
+                />).toJS()}
+              </MessagesList>
+            </Tab>
+          </Tabs>
+        </View>
       </Container>
     )
   }
